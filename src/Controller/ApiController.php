f4e0e7bfa0e21431af3d4a0e0de49aaeb92b4426
seb974 <m_seb@icloud.com>
2022-02-13 10:34:24 +0400

    code needed to upgrade database created

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,8 +10,11 @@
  */
 namespace App\Controller;
 
+use App\Repository\OrderEntityRepository;
 use App\Repository\PlatformRepository;
+use App\Repository\ProductRepository;
 use App\Repository\ProvisionRepository;
+use App\Repository\StockRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -31,17 +34,45 @@ class ApiController extends AbstractController
     }
 
     /**
-     * @Route("/moulinette-stocks", name="stocks_link", methods={"GET"})
+     * @Route("/moulinette-products", name="products_stocks_link", methods={"GET"})
      *
      */
-    public function updateProvisions(ProvisionRepository $provisionRepository, PlatformRepository $platformRepository): JsonResponse
+    public function updateProducts(ProductRepository $productRepository): JsonResponse
     {
-        $platform = $platformRepository->find(1);
-        $provisions = $provisionRepository->findAll();
-        foreach ($provisions as $provision) {
-            $provision->setPlatform($platform);
+        $products = $productRepository->findAll();
+        foreach ($products as $product) {
+            if (!is_null($product->getStock())) {
+                $product->addStock($product->getStock());
+            } else {
+                if (!is_null($product->getVariations() && count($product->getVariations()) > 0)) {
+                    foreach ($product->getVariations() as $variation) {
+                        foreach ($variation->getSizes() as $size) {
+                            $size->addStock($size->getStock());
+                        }
+                    }
+                }
+            }
         }
         $this->getDoctrine()->getManager()->flush();
-        return new JsonResponse($provisions);
+        return new JsonResponse($products);
+    }
+
+    /**
+     * @Route("/moulinette-entities", name="entities_stocks_link", methods={"GET"})
+     *
+     */
+    public function updateEntities(ProvisionRepository $provisionRepository, PlatformRepository $platformRepository, OrderEntityRepository $orderRepository, StockRepository $stockRepository): JsonResponse
+    {
+        $platform = $platformRepository->find(1);
+        $stocks = $stockRepository->findAll();
+        $provisions = $provisionRepository->findAll();
+        $orders = $orderRepository->findAll();
+        $entities = array_merge($stocks, $orders, $provisions);
+
+        foreach ($entities as $entity) {
+            $entity->setPlatform($platform);
+        }
+        $this->getDoctrine()->getManager()->flush();
+        return new JsonResponse($entities);
     }
 }
